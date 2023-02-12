d1b3ca0c61c6018e5b820079ab537c89bf23fd39
yuntaeLee <leeyt1201@gmail.com>
2023-02-13 06:02:29 +0900

    [#1] feat: Product

---
+++ product-service/src/main/java/com/msa/productservice/service/ProductService.java
@@ -0,0 +1,46 @@
+package com.msa.productservice.service;
+
+import com.msa.productservice.dto.ProductRequest;
+import com.msa.productservice.dto.ProductResponse;
+import com.msa.productservice.model.Product;
+import com.msa.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description(productRequest.getDescription())
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll().stream()
+                .map(this::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+}
