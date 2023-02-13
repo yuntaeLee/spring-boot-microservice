5c290cfe34baa8365e6e6fe4997f10e778ed004c
yuntaeLee <leeyt1201@gmail.com>
2023-02-13 10:40:13 +0900

    [#4] feat: Synchronize order and inventory close #4

--- order-service/src/main/java/com/msa/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/msa/orderservice/service/OrderService.java
@@ -1,5 +1,7 @@
 package com.msa.orderservice.service;
 
+import com.msa.orderservice.config.WebClientConfig;
+import com.msa.orderservice.dto.InventoryResponse;
 import com.msa.orderservice.dto.OrderLineItemsDto;
 import com.msa.orderservice.dto.OrderRequest;
 import com.msa.orderservice.model.Order;
@@ -8,7 +10,9 @@ import com.msa.orderservice.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -19,6 +23,7 @@ import java.util.stream.Collectors;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final WebClient webClient;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -30,14 +35,34 @@ public class OrderService {
                 .collect(Collectors.toList());
 
         order.setOrderLineItems(orderLineItems);
-        orderRepository.save(order);
+
+        List<String> skuCodes = order.getOrderLineItems().stream()
+                .map(OrderLineItems::getSkuCode)
+                .collect(Collectors.toList());
+
+        // Call Inventory Service, nad place order if product is in stock
+        InventoryResponse[] inventoryResponseArray = webClient.get()
+                .uri("http://localhost:8082/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+                .retrieve()
+                .bodyToMono(InventoryResponse[].class)
+                .block();
+
+        boolean allProductsInStock = Arrays.stream(inventoryResponseArray)
+                .allMatch(InventoryResponse::isInStock);
+
+        if (allProductsInStock) {
+            orderRepository.save(order);
+        } else {
+            throw new IllegalArgumentException("Product is not in stock, please try again later");
+        }
     }
 
     private OrderLineItems mapToDto(OrderLineItemsDto orderLineItemsDto) {
         OrderLineItems orderLineItems = new OrderLineItems();
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
-        orderLineItems.setSkuCode(orderLineItems.getSkuCode());
+        orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
         return orderLineItems;
     }
 }
